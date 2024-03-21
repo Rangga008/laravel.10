f25e085c549aedaf69c238b37149dce71d6b4e17
Rangga008 <rizk.rangga09@gmail.com>
2024-03-21 08:56:23 +0700

    Added by Rangga

--- public/index.php
+++ public/index.php
@@ -1,7 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<body>
+use Illuminate\Http\Request;
 
-    <h1>HEllo</h1>
-</body>
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
